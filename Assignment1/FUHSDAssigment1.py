0072c05b5577a31c5bd7b59c5f13675558a73b0c
Yosh Kashima <39385549+yoshkashima@users.noreply.github.com>
2018-09-19 17:20:46 -0700

    Initial commit

---
+++ Assignment1/FUHSDAssigment1.py
@@ -0,0 +1,11 @@
+# Python for Data Science
+# Yosh Kashima  9/16/2018
+state_name = "California"
+state_name = "Texas"
+state_name = "Washington"
+if state_name == "California":
+    print("Golden State")
+elif state_name == "Texas":
+    print("Lone Star State")
+else:
+    print("Not a Golden State or Lone Star State")
